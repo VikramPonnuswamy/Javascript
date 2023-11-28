21919335eaeb11d1831f0bab28d6b4d974f31a2a
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-28 20:40:29 +0000

    tried the highest and lowest question

--- JS Objects Exercise/Object Exercise/highestlowest.js
+++ JS Objects Exercise/Object Exercise/highestlowest.js
@@ -1,5 +1,10 @@
 function highestlowest(input) {
     // accepts a string as input, which will be space separated numbers, and you must return the highest and lowest number in the string as a space separated string, highest number first
+    input = ["1", "9", "3", "4", "-5"];
+    input.sort();
+    let endIndex = input.length;
+    return input[1] + " " + input[endIndex];
+
 }
 
 module.exports = highestlowest;
