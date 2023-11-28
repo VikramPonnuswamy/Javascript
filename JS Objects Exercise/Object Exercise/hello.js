388a72332b786ec171046e92e685455af8392b24
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-28 19:43:54 +0000

    tried some example

--- JS Objects Exercise/Object Exercise/hello.js
+++ JS Objects Exercise/Object Exercise/hello.js
@@ -1,6 +1,16 @@
+// This is what is given
+
+// function hello(name) {
+//   // should return "hello name"
+//   return -1;
+// }
+
+// module.exports = hello;
+
+//Answer to fix the code. 
 function hello(name) {
   // should return "hello name"
-  return -1;
+  return "hello " + name;
 }
 
 module.exports = hello;
