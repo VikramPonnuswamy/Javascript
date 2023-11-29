ee766c5f2a018047fab81ce8c1a61769fc31a501
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-29 16:00:11 +0000

    answered highlowest.js question

--- JS Objects Exercise/Object Exercise/highestlowest.js
+++ JS Objects Exercise/Object Exercise/highestlowest.js
@@ -1,9 +1,14 @@
 function highestlowest(input) {
     // accepts a string as input, which will be space separated numbers, and you must return the highest and lowest number in the string as a space separated string, highest number first
-    input = ["1", "9", "3", "4", "-5"];
-    input.sort();
-    let endIndex = input.length;
-    return input[1] + " " + input[endIndex];
+   
+    //this line splits the string into an array everytime it detects a space. so each word is stored in its own index
+    let string = input.split(" ")
+    
+    //this code takes the first index as a and another index as b and compares to see which is higher and this is done throughout the array sorts from largest to smallest. a-b will do smallest to largest. 
+    string.sort(function (a,b){return b-a})
+
+    //this returns the first value aka highest and split by a space and returns the smallest ie items that's present in the last index.
+    return string[0] + " " + string[string.length-1];
 
 }
 
