03513aff34540b9be384eae329e24f6221cc7b93
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-29 11:26:17 +0000

    finished exercise sumall35.js

--- JS Objects Exercise/Object Exercise/sumall35.js
+++ JS Objects Exercise/Object Exercise/sumall35.js
@@ -1,8 +1,27 @@
 function sumall35(high) {
   // should return the sum of all multiples of 3 and 5 below high, or -1 if high is not a number
+  
   if (typeof high != "number") {
+
     return -1;
   }
+
+  let value = high - 1;
+  let finalValue = 0;
+  while (value > 0) {
+    
+    if (value % 3 == 0 || value % 5 == 0) {
+      finalValue = finalValue + value;
+      value--;
+    }
+    else {
+      value--;
+    }
+
+  }
+    return finalValue ; 
+  
+  
 }
 
 module.exports = sumall35;
