b99802ff241891e2be409cbbfae437d88beee846
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-29 11:47:50 +0000

    completed largest.js exercise

--- JS Objects Exercise/Object Exercise/largest.js
+++ JS Objects Exercise/Object Exercise/largest.js
@@ -1,6 +1,7 @@
 function largest_in_list(arr) {
   // returns the largest value in the supplied list
-  return -1;
+  arr.sort(function(a,b){return b-a});
+  return arr[0];
 }
 
 module.exports = largest_in_list;
