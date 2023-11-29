820e7691186a45042b03e8431bf8cedd698d33d8
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-29 16:06:50 +0000

    answered and completed evenorodd.js question

--- JS Objects Exercise/Object Exercise/evenorodd.js
+++ JS Objects Exercise/Object Exercise/evenorodd.js
@@ -1,6 +1,15 @@
 function even_or_odd(num) {
   // returns 'even' if the number given is even, or 'odd' if the number given is odd.
-  return undefined;
+  // this checks if the number is has any remainder when it is divided by 2. if it doesn't the number is even. hence printing even. otherwise it's odd. 
+  
+  if (num % 2 ==0) {
+    return "even" ;
+  }
+  else {
+    return "odd" ;
+  }
+  
+  
 }
 
 module.exports = even_or_odd;
