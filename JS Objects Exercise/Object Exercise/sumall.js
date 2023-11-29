5ecdbdbd79d42a1458ba6c555db2c2006717dbfe
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-29 11:06:29 +0000

    completed exercise on sumall.js

--- JS Objects Exercise/Object Exercise/sumall.js
+++ JS Objects Exercise/Object Exercise/sumall.js
@@ -1,6 +1,17 @@
+//let num = 0;
+
+//This function takes in the num value. 
 function sumall(num) {
   // returns the sum of all numbers smaller than or equal to num
-  return -1;
+  let sumValue = 0;
+
+  //loop is ran until num hits 0. you add num to the sum Value then subtract -1 from num and repeats 
+  while (num > 0) {
+    sumValue = sumValue + num;
+    num--;
+  }
+  return sumValue;
+ 
 }
 
 module.exports = sumall;
