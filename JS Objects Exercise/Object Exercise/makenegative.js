bb6102d8a89097a316ef3bb9761d10606ddd9245
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-30 11:07:38 +0000

    completed question makenegative.js

--- JS Objects Exercise/Object Exercise/makenegative.js
+++ JS Objects Exercise/Object Exercise/makenegative.js
@@ -1,5 +1,13 @@
 function make_negative(num) {
   // returns the negative value of the number
+  
+  if (num < 0) {
+    return num;
+  }
+  else {
+    let value = num * -1;
+    return value;
+  }
 }
 
 module.exports = make_negative;
