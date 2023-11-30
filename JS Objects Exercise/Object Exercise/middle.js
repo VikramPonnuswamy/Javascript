3478413cfaeb55474e2e306f8b8509eb87cd0184
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-11-30 20:45:20 +0000

    compeleted middle.js exercise

--- JS Objects Exercise/Object Exercise/middle.js
+++ JS Objects Exercise/Object Exercise/middle.js
@@ -1,5 +1,20 @@
 function middle(input) {
     // accepts a string as input, returns the middle character (if length is odd) or the middle two characters (if length is even)
+    let inputArray = input.split("");
+    let middleValue = inputArray.length / 2;
+
+    if (inputArray.length % 2 == 0) {
+        
+        return inputArray[middleValue-1] + inputArray[middleValue];
+    }
+    else if (inputArray.length < 1 ) {
+        return inputArray;
+    }
+    else {
+        return inputArray[middleValue-0.5];
+    }
+
+    
 }
 
 module.exports = middle;
