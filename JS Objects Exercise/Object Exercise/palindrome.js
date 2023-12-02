6500d62f1df7a89cec7aa3cf3a0d45bd15021b7b
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-12-02 18:11:09 +0000

    Completed exercise plindrome.js

--- JS Objects Exercise/Object Exercise/palindrome.js
+++ JS Objects Exercise/Object Exercise/palindrome.js
@@ -1,6 +1,17 @@
 function is_palindrome(word) {
   // returns true if word is a palindrome, false otherwise
-  return -1;
+  
+  let wordArray = word.split('');
+
+  let reverseWordArray = wordArray.reverse();
+  let joinReverseWordArray = reverseWordArray.join('');
+  
+  if (joinReverseWordArray == word) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = is_palindrome;
