6a455a0158287db285178a05c3625c2f88f12b75
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-12-02 20:10:28 +0000

    adding assingment file

---
+++ University Assignment/2324_CMT120_CW1_TEMPLATE/python/template.py
@@ -0,0 +1,41 @@
+import copy
+
+# Exercise 1 - Smallest Fraction Terms
+def exercise1(num,den):
+    return None
+
+# Exercise 2 - Magical Dates
+def exercise2(day,month,year):
+    return None
+
+# Exercise 3 - All Sublists
+def exercise3(l):
+    return None
+
+# Exercise 4 - English to Pig Latin Translator
+def exercise4(word):
+    return None
+
+# Exercise 5 - Morse Code Encoder
+def exercise5(message):
+    return None
+
+# Exercise 6 - Spelling Out Numbers
+def exercise6(num):
+    return None
+
+# Exercise 7 - No Functions without Comments
+def exercise7(filename):
+    return None
+
+# Exercise 8 - Justify any Text
+def exercise8(filename,length):
+    return None
+
+# Exercise 9 - Knight's Challenge
+def exercise9(start,end,moves):
+    return None
+
+# Exercise 10 - War of Species
+def exercise10(environment):
+    return None
