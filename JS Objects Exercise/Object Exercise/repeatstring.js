c20c8e4fb7c6f03f4cb7a54b1386c30cf1bcf8fa
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-12-02 18:41:27 +0000

    completed exercise repeatestring.js

--- JS Objects Exercise/Object Exercise/repeatstring.js
+++ JS Objects Exercise/Object Exercise/repeatstring.js
@@ -1,5 +1,11 @@
 function repeat_string(s, n) {
   // returns a string with the string s repeated n times
+  let string = '';
+  while (n > 0) {
+    string = string.concat(s);
+    n--;
+  }
+  return string;
 }
 
 module.exports = repeat_string;
