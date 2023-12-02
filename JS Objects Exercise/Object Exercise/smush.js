dbc8b5b31e6dcb95b10cae7b752088ca09a27fd4
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-12-02 19:34:18 +0000

    completed smush.js

--- JS Objects Exercise/Object Exercise/smush.js
+++ JS Objects Exercise/Object Exercise/smush.js
@@ -1,5 +1,16 @@
 function smush(a, b) {
   // merges the two lists a and b by taking alternate elements from each list in turn
+  let newArray = [];
+
+ for (i=0;i<a.length;i++) {
+
+  //This push functions pushes the value at the end of the new array.
+  newArray.push(a[i]);
+  newArray.push(b[i])
+  
+ }
+
+ return newArray;
 
 }
 
