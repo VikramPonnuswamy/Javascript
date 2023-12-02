4d69d581bce20af2f0c3ee07460a1f354fb7412b
VikramPonnuswamy <vikram.ponnuswamy@outlook.com>
2023-12-02 19:52:39 +0000

    compeleted population.js

--- JS Objects Exercise/Object Exercise/population.js
+++ JS Objects Exercise/Object Exercise/population.js
@@ -1,5 +1,17 @@
 function population(start, increase, additions, target) {
     // given a starting population, which increases by increase percent each year, and additions people arriving from outside town, how many years does it take to reach the target population?
+
+let currentPop = start;
+let year = 0;
+
+//This is just simple maths to be honest. in a while loop cuz we don't know the number of iterations.
+while (currentPop < target) {
+    currentPop = currentPop*(1+(increase/100)) + additions;
+    year++;
+}
+
+return year;
+
 }
 
 module.exports = population;
